1dbf7ba4dd3a50106f93e4fbaa07f279012c227c
Jodi Tivis <joditivis@gmail.com>
2020-02-07 16:27:00 -0700

    added more content, aos animations, bootstrap, materialize

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+// mobile navbar menu 
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+
+    // aos slide in affect initialization
+    AOS.init();
+  });
+
+// Slick Carousel
+$('.gallery-responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
